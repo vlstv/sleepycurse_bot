89225d348136e704149364f6983b515a4d74763c
vlstv <svolstov@hmail.com>
2018-12-16 22:19:38 +0200

    working staff

--- bandcamp.py
+++ bandcamp.py
@@ -8,25 +8,49 @@ import wgetter
 from lxml import html
 import os
 import time
+import flask
+
+
+app = Flask(__name__)
+sslify = SSLify(app)
+TOKEN = '460119009:AAFbL_pJJtqBaBMMjODKYPS-7Xk_p_swS1M'
+bot = telebot.TeleBot(TOKEN, threaded=False)
 
 
 
-url = 'https://mountainmanrecords.bandcamp.com/track/solitude'
+@app.route('/', methods=['POST','GET'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
 
-response = requests.get(url)
-tree = html.fromstring(response.content)
 
-cover_url = tree.xpath('//img[@itemprop="image"]/@src')[0]
-meta = tree.xpath('//meta[@name="title"]/@content')[0]
+@bot.message_handler(content_types=['text'])
+def handle_start(message):
 
-response = response.text
-audio_url = response.split('"file":{"mp3-128":"')[-1].split('"}')[0]
-meta = meta.split(", by ")
-track = meta[0]
-artist = meta[1]
-tmp_file = response.split('/mp3-128/')[-1].split('?')[0]
 
-def download(audio_url, cover_url, artist, track, tmp_file):
+    #url = 'https://mountainmanrecords.bandcamp.com/track/solitude'
+
+    url = message.text
+
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+
+    cover_url = tree.xpath('//img[@itemprop="image"]/@src')[0]
+    meta = tree.xpath('//meta[@name="title"]/@content')[0]
+
+    response = response.text
+    audio_url = response.split('"file":{"mp3-128":"')[-1].split('"}')[0]
+    meta = meta.split(", by ")
+    track = meta[0]
+    artist = meta[1]
+    tmp_file = response.split('/mp3-128/')[-1].split('?')[0]
+
+
 
     dirc = '/home/vlstv/%s' % artist
     os.mkdir(dirc)
@@ -40,17 +64,33 @@ def download(audio_url, cover_url, artist, track, tmp_file):
     old_file = '%s/%s' % (dirc, tmp_file)
     new_file = '%s/%s - %s.mp3' % (dirc, artist, track)
 
-    print(old_file)
-    print(new_file)
 
     try:
         wgetter.download(cover_url, outdir='/home/vlstv/%s' % artist)
     except:
-        #send error message
         print('error')
 
     os.rename(old_file, new_file)
 
-download(audio_url, cover_url, meta[1], track, tmp_file)
+    #send files to chat
+    files = os.listdir(dirc)
+    for file_path in files:
+        if '.mp3' not in file_path:
+            img_url = '%s/%s' % (dirc, file_path)
+
+    #send cover
+    file = open(img_url, 'rb')
+    bot.send_photo(message.chat.id, file)
+    os.remove(img_url)
+
+    #send audio
+    file = open(new_file, 'rb')
+    bot.send_audio(message.chat.id, file)
+    os.remove(new_file)
+    os.rmdir(dirc)
 
 
+
+if __name__ == '__main__':
+    #app.run(ssl_context=('/etc/letsencrypt/live/shmuli.tk/fullchain.pem', '/etc/letsencrypt/live/shmuli.tk/privkey.pem'))
+    app.run()
