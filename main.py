5db5ea6bf66c4fba565da0e4792d1ffea1dbc299
Sergei Volosatov <32194548+vlstv@users.noreply.github.com>
2021-11-14 11:45:00 +0300

    full project rewrite

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import requests
 import wgetter
 from lxml import html
 import os
-import time
 import flask
 import re
-import sys
-import mutagen
-from mutagen.easyid3 import EasyID3
-from local_settings import *
+import html as uhtml
+import json
+from settings import GROUP_ID, TOKEN, WEBHOOK_URL
+
+download_dir ='/tmp'
 
 app = Flask(__name__)
 bot = telebot.TeleBot(TOKEN, threaded=False)
@@ -26,102 +26,64 @@ def webhook():
     else:
         flask.abort(403)
 
+def get_meta(response):
+    tree = html.fromstring(response.text)
+    cover = tree.xpath('//meta[@property="og:image"]/@content')[0]
+    meta = tree.xpath('//meta[@name="title"]/@content')[0]
+    meta = meta.split(', by ')
+    return {'artist': meta[1], 'album': meta[0], 'cover': cover}
+
+def get_song_list(response):
+    response = uhtml.unescape(response.text)
+    songs  = re.findall('trackinfo\":[^]]*]', response)[0].replace('trackinfo\":', '')
+    songs = json.loads(songs)
+    song_list = []
+    for song in songs:
+        try:
+            song_list.append({0: [song["title"], song["file"]["mp3-128"]]})
+        except:
+            pass
+    return song_list
+
+def get_songs(url, chat_id):
+    response = requests.get(url)
+    info = get_meta(response)
+    song_list = get_song_list(response)
+    info.update({'songs': song_list})
+    return info
+
 @bot.message_handler(content_types=['text'])
 def handle_start(message):
 
-    #check if url is valid 
+    #check if url is valid
     if re.match(r'https:\/\/*.*\.bandcamp\.com\/track\/*.*', message.text):
-        
-        url = message.text
-
-        try:    
-            response = requests.get(url)
-            tree = html.fromstring(response.content)
-            #get song cover
-            cover_url = tree.xpath('//img[@itemprop="image"]/@src')[0]
-            #get song and artist from metadata
-            meta = tree.xpath('//meta[@name="title"]/@content')[0]
-
-            response = response.text
-            #get audio url
-            audio_url = response.split('"file":{"mp3-128":"')[-1].split('"}')[0]
-            meta = meta.split(", by ")
-            track = meta[0] #track name
-            artist = meta[1] #artist name
-            tmp_file = response.split('/mp3-128/')[-1].split('?')[0]
-        except:
-	    sys.exit()
-
-
-        dirc = UPLOAD_DIR + artist
-        #create artists's dir
         try:
-            os.mkdir(dirc)
-            #download audio to created dir
-            wgetter.download(audio_url, outdir=dirc)
-        except:
-            bot.send_message(message.chat.id, 'error occured while downloading track')
-
-        old_file = '%s/%s' % (dirc, tmp_file)
-        #create new name for downloaded file:
-        new_file = '%s/%s - %s.mp3' % (dirc, artist, track)
+            url = message.text
+            meta = get_songs(url, message.chat.id)
+            artist = meta['artist']
+            cover_url = meta['cover']
+            album = meta['album']
+            song_url = meta['songs'][0][0][1]
+            song = meta['songs'][0][0][0]
 
 
-        try:
-            #download song cover
-            wgetter.download(cover_url, outdir=dirc)
-        except:
-            bot.send_message(message.chat.id, 'error occured while downloading image')
-        #rename audio file
-        os.rename(old_file, new_file)
-        
-        try:
-            audiofile = EasyID3(new_file)
-            audiofile['artist'] = artist
-            audiofile['title'] = track
-            audiofile.save()
-        except mutagen.id3.ID3NoHeaderError:
-            audiofile = mutagen.File(new_file, easy=True)
-            audiofile.add_tags()
-            audiofile['artist'] = artist
-            audiofile['title'] = track
-            audiofile.save()
+            wgetter.download(song_url, outdir=download_dir)
+            wgetter.download(cover_url, outdir=download_dir)
 
-        #send files to chat
-        files = os.listdir(dirc)
-        for file_path in files:
-            if '.mp3' not in file_path:
-                img_url = '%s/%s' % (dirc, file_path)
+            audio_file = download_dir + '/' + song_url.split('/mp3-128/')[-1].split('?')[0]
+            cover_file = download_dir + '/' + cover_url.split('img/')[1]
 
-        #send cover
-        try:
-            file = open(img_url, 'rb')
-            bot.send_photo(BOT_TAG, file)
-            #delete cover after sending
-            os.remove(img_url)
+            cover = open(cover_file, 'rb')
+            bot.send_photo(GROUP_ID, cover, caption='{} - {}'.format(artist, album), disable_notification=True)
+            os.remove(cover_file)
 
-            #send audio
-            file = open(new_file, 'rb')
-            bot.send_audio(BOT_TAG, file)
-            #delete audio and folser after sending 
-            os.remove(new_file)
-            os.rmdir(dirc)
-        except:
-            try:
-                #delete all files if upload fails 
-                os.remove(img_url)
-                os.remove(new_file)
-                os.rmdir(dirc)
-            except:
-                print('upload failed')
-
-
-    else:
-        bot.send_message(message.chat.id, 'url is not valid')
-
+            audio = open(audio_file, 'rb')
+            bot.send_audio(GROUP_ID, audio, performer=artist, title=song, disable_notification=False)
+            os.remove(audio_file)
+        except Exception as e:
+            bot.send_message(message.chat.id, e)
 
 bot.set_webhook(WEBHOOK_URL)
 
 if __name__ == '__main__':
-    app.run(ssl_context=(SSL_FULLCHAIN, SSL_PRIVKEY))
     app.run()
